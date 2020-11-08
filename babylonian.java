732d4bea0f0bf9d97705b784ec545861aa59036d
Daniel <47431549+Danvs60@users.noreply.github.com>
2020-11-08 16:20:35 +0000

    Update babylonian.java

--- babylonian.java
+++ babylonian.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class submission1 {
+public class babylonian {
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		float n = s.nextFloat();
